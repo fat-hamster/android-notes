f5dc33086bd7d17a15a495113feeeb54b6c75264
Dmitriy Gridchin <tigris.tstu@gmail.com>
2021-04-29 18:47:02 +0300

    Завершил. Ошибка была в этой строчке [android:background="@drawable/side_nav_bar"]

--- app/src/main/java/com/dmgpersonal/notes/MainActivity.java
+++ app/src/main/java/com/dmgpersonal/notes/MainActivity.java
@@ -42,23 +42,9 @@ public class MainActivity extends AppCompatActivity {
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(item -> {
-            int id = item.getItemId();
-
-            switch (id) {
-                case R.id.action_add:
-                    Toast.makeText(getApplicationContext(), "Add new note", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_edit:
-                    Toast.makeText(getApplicationContext(), "Edit note", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_settings:
-                    Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
-                    return true;
-                case R.id.action_about:
-                    Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
-                    return true;
+            if(menuChosen(item.getItemId())) {
+                return true;
             }
-
             return false;
         });
     }
@@ -70,20 +56,33 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    @SuppressLint("NonConstantResourceId")
+    private Boolean menuChosen(int id) {
+        switch (id) {
+            case R.id.action_add:
+                Toast.makeText(getApplicationContext(), "Add new note", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_edit:
+                Toast.makeText(getApplicationContext(), "Edit note", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_settings:
+                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_about:
+                Toast.makeText(getApplicationContext(), "About", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+
     // Из-за того, что switch-case требует для case использовать константы
     // приходиться использовать аннотацию NonConstantResourceId, либо заменять switch на if-else if,
     // что снижает читаемость. http://tools.android.com/tips/non-constant-fields
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int id = item.getItemId();
-        switch (id) {
-            case R.id.action_add:
-                Toast.makeText(getApplicationContext(), "Add new note", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.action_settings:
-                Toast.makeText(getApplicationContext(), "Settings", Toast.LENGTH_SHORT).show();
-                return true;
+        if(menuChosen(item.getItemId())) {
+            return true;
         }
         return super.onOptionsItemSelected(item);
     }
